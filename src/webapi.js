b84b9825f8e4994151f04aa1f2b7361d5388310d
endb <56726835+endb@users.noreply.github.com>
2021-01-20 03:07:22 +0300

    Create webapi.js

---
+++ src/webapi.js
@@ -0,0 +1,31 @@
+/*
+	props: {
+		pageSize
+		currentPage
+		search
+		order
+	}
+
+	C# : methodXYZ(string search, int top, int skip, string orderBy, string orderdir)
+*/
+export default function param(uri, header, props) {
+	const url = new URL(uri);
+	//
+	// Paging //
+	if (props.pageSize) {
+		url.searchParams.append('skip', props.pageSize * (props.currentPage - 1));
+		url.searchParams.append('top', props.pageSize);
+	}
+
+	// Filter //
+	if (props.search)
+		url.searchParams.append('search', props.search);
+
+	// Order //
+	if (props.order.length > 0) {
+		url.searchParams.append('orderby', props.order.map(m => { return props.cols[m.index].name }).join(','));
+		url.searchParams.append('orderdir', props.order.map(m => { return m.type ? m.type : 'asc' }).join(','));
+	}
+
+	return new Request(url, header);
+}
